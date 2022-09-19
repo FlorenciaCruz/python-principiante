0246f2e8a13d7c919d5dd9c70773332da5f26501
Florencia Cruz <112032348+FlorenciaCruz@users.noreply.github.com>
2022-09-19 16:17:31 -0300

    guia 1
    agrego comentarios y prueba de escritorio

--- guia 1/ejercicio17_guia1.py
+++ guia 1/ejercicio17_guia1.py
@@ -3,12 +3,22 @@ numero1=int
 numero2=int
 numero=int
 suma=int
-numero1=int(input("ingrese el numero1: "))
-numero2=int(input("ingrese el numero2: "))
-numero3=int(input("ingrese el numero3: "))
-suma=numero1+numero2+numero3
-print("la suma de todos los numeros es: ",suma)
-if(suma>10):
-    print("division:",round(suma/2))
+numero1=int(input("ingrese el numero1: "))#ingreso numero 1 por teclado
+numero2=int(input("ingrese el numero2: "))#ingreso numero 2 por teclado
+numero3=int(input("ingrese el numero3: "))#ingreso numero 3 por teclado
+suma=numero1+numero2+numero3#suma de todos los numeros
+print("la suma de todos los numeros es: ",suma)#imprimo los resultados
+if(suma>10):#si la suma de todos los numeros es mayor a 10
+    print("division:",round(suma/2))#divido por 2
 else:
-    print("potencia",suma*suma*suma)
+    print("potencia",suma*suma*suma)#elevo al cubo
+"""********************************************************************************************************
+                                    PRUEBA DE ESCRITORIO
+ **********************************************************************************************************
+  INTENTO      ENTRADA                      SALIDA
+ **********************************************************************************************************
+ *     1     * ingrese el numero1: 2  *   la suma de todos los numeros es:  9                                        
+ *           * ingrese el numero2: 4  *     potencia 729
+ *           * ingrese el numero3: 3  *                                                                                                                    
+ * ********************************************************************************************************
+ *"""
