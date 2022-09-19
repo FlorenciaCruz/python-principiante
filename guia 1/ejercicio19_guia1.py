0b1d282c2b12678740fe2289db2c39df486dc0d5
Florencia Cruz <112032348+FlorenciaCruz@users.noreply.github.com>
2022-09-19 16:19:41 -0300

    guia 1
    agrego correccion y comentarios

--- guia 1/ejercicio19_guia1.py
+++ guia 1/ejercicio19_guia1.py
@@ -8,16 +8,29 @@ los cuadros son anteriores al siglo XX
  en el año 1901 y terminó en el año 2000).
 Determinar cuántos tienen antigüedad inferior
 a 10 años. Si no hay ninguno, imprimir el mensaje “Renovar stock”."""
+#declaracion de variables
+cuadro=0
+contador=1
+while(contador<=3):
+    cuadro=int(input("ingrese el año del cuadro: "))
+    if((cuadro>1901)and(cuadro<2000)):
+        print("el cuadro es anterior al siglo 20")
+    if (cuadro<1891):
+        print("antiguedad menor a 10 años del sigo 20")   
+    if(cuadro>2000):
+        print("renovar stock")
+    contador=contador+1
 
-cuadro1=int
-cuadro2=int
-cuadro3=int
-cuadro1=int(input("ingrese el cuadro 1:"))
-cuadro2=int(input("ingrese el cuadro 2:"))
-cuadro3=int(input("ingrese el cuadro 3:"))
-if((cuadro1<2000)or(cuadro1>1901))or((cuadro2<2000)or(cuadro>1901))or((cuadro3<2000)or(cuadro3>2000)):
-    print("el cuadro es anteior al siglo 20")
-if ((cuadro1<1891)or(cuadro2<1891)or(cuadro3<1891)):
-    print("antiguedad menor a 10 años del sigo 20")
-else:
-    print("renovar stock")
+"""********************************************************************************************************
+                                    PRUEBA DE ESCRITORIO
+ **********************************************************************************************************
+  INTENTO       ENTRADA                                         SALIDA    
+ **********************************************************************************************************
+ *     1    *     ingrese el año del cuadro: 2022             *renovar stock
+ *----------*-------------------------------------------------*-------------------------------------------      
+ *          *     ingrese el año del cuadro: 1700             *antiguedad menor a 10 años del sigo 20
+ *----------*-------------------------------------------------*-------------------------------------------                                                   
+ *          *     ingrese el año del cuadro: 1980             *el cuadro es anterior al siglo 20 
+ *----------*-------------------------------------------------*-------------------------------------------
+ * ********************************************************************************************************                        
+ *"""
