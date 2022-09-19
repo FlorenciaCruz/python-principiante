3f5ae79a2539bcca8d0c360c89194f476f31dddc
Florencia Cruz <112032348+FlorenciaCruz@users.noreply.github.com>
2022-09-19 16:12:14 -0300

    guia 1
    agrego comentarios y prueba de escritorio

--- guia 1/ejercicio12_guia1.py
+++ guia 1/ejercicio12_guia1.py
@@ -18,12 +18,26 @@ division
 cociente
 resto
 _________________"""
-
-primer_numero=int(input("ingrese el primer numero: "))
-segundo_numero=int(input("ingrese el segundo numero: "))
-division=primer_numero/segundo_numero;
-cociente=primer_numero//segundo_numero;
-resto=primer_numero%segundo_numero;
+#inicio del programa
+primer_numero=int(input("ingrese el primer numero: "))#ingreso primer numero por teclado
+segundo_numero=int(input("ingrese el segundo numero: "))#ingreso segundo numero por teclado
+division=primer_numero/segundo_numero;#almacena la division del primer numero por el segundo numero
+cociente=primer_numero//segundo_numero;#almacena el cociente del primer numero y el segundo numero
+resto=primer_numero%segundo_numero;#almacena el resto del primer y segundo numero
+#imprime los resultados por pantalla
 print("la division del primer numero ",primer_numero,"por el segundo numero ",segundo_numero,"es de",division)
 print("el cociente del primer numero ",primer_numero,"por el segundo numero ",segundo_numero,"es de",cociente)
 print("el resto del primer numero: ",primer_numero,"por el segundo numero: ",segundo_numero,"es de",resto)
+"""********************************************************************************************************
+                                    PRUEBA DE ESCRITORIO
+ **********************************************************************************************************
+  INTENTO            ENTRADA                                SALIDA
+ **********************************************************************************************************
+ *     1     *      ingrese el primer numero: 120   *       la division del primer numero  120 por el segundo numero  20 es de 6.0
+ *           *      ingrese el segundo numero: 20   *       el cociente del primer numero  120 por el segundo numero  20 es de 6  
+ *           *                                      *       el resto del primer numero:  120 por el segundo numero:  20 es de 0 
+ *           *                                      *                                                                
+ *           *                                      *        
+ *           *                                      *          
+ * ********************************************************************************************************
+ *"""
