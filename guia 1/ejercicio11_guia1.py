e6d5c86911a687cef09521b14b150929aa7b9dd1
Florencia Cruz <112032348+FlorenciaCruz@users.noreply.github.com>
2022-09-19 16:11:15 -0300

    guia 1
    agrego comentarios y prueba de escritorio

--- guia 1/ejercicio11_guia1.py
+++ guia 1/ejercicio11_guia1.py
@@ -14,10 +14,32 @@ contador
 numero
 tabla
 ___________________"""
-contador=int
-contador=0
-numero=(int(input("ingrese un numero y se mostrata su tabla de multiplicar hasta el 10")))
-while(contador<=10):
-    contador=contador+1;
-    tabla=contador*numero;
-    print(contador,"x",numero,"=",tabla)
+"declaracion de variables"
+contador=int#clase entero
+contador=0#inicializo en 0
+"inicializacion del programa"
+numero=(int(input("ingrese un numero y se mostrata su tabla de multiplicar hasta el 10: ")))#ingreso numero por teclado
+while(contador<=9):#se repite el bucle hasta que contador sea 9
+    contador=contador+1;#contador
+    tabla=contador*numero;#es el contador por el numero ingresado anteriormente, de esta manera muestra la tabla del 1 al 10
+    print(contador,"x",numero,"=",tabla)#muestro la tabla por pantalla
+"""********************************************************************************************************
+                                    PRUEBA DE ESCRITORIO
+ **********************************************************************************************************
+  INTENTO         ENTRADA                                                                   SALIDA
+ **********************************************************************************************************
+ *     1         *  ingrese un numero y se mostrata su tabla de multiplicar hasta el 10: 1*        
+ *               *                                                                        *  1 x 1 = 1
+ *               *                                                                        *  2 x 1 = 2
+ *               *                                                                        *  3 x 1 = 3
+ *               *                                                                        *  4 x 1 = 4
+ *               *                                                                        *  5 x 1 = 5
+ *               *                                                                        *  6 x 1 = 6
+ *               *                                                                        *  7 x 1 = 7
+ *               *                                                                        *  8 x 1 = 8
+ *               *                                                                        *  9 x 1 = 9
+ *               *                                                                        *  10 x 1 = 10                                                                                        
+ *               *                                                                        *
+ *               *                                                                        *
+ * ********************************************************************************************************
+ *"""
