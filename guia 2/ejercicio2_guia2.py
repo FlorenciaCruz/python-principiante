8d440948d09f9150f8dc66507c110033a1b8d447
Florencia Cruz <112032348+FlorenciaCruz@users.noreply.github.com>
2022-09-20 06:58:33 -0300

    guia 2
    agrego comentarios y prueba de escritorio

--- guia 2/ejercicio2_guia2.py
+++ guia 2/ejercicio2_guia2.py
@@ -1,21 +1,44 @@
 "Secuencia de impares. Cargar por teclado dos números, e imprimir los números impares que se encuentran comprendidos entre ellos, en forma ascendente y descendente."
 """entrada
 ____________________
+numero1
+numero2
 ____________________
 proceso
 ____________________
+resto = i % 2
 ____________________
 salida
 ____________________
+i
 ____________________"""
+#declaracion de variables
 resto=int
 i=int
-numero1=int(input("ingrese un numero: "))
-numero2=int(input("ingrese otro numero: "))
-for i in range(numero1,numero2+1):
-    if(i%2!=0):
-       print(i)
+#inicio del programa
+numero1=int(input("ingrese un numero: "))#ingreso numero1 por teclado
+numero2=int(input("ingrese otro numero: "))#ingreso numero2 por teclado
+#forma decendente
+for i in range(numero1,numero2+1):#numeros comprendidos entre numero1 y numero2
+    if(i%2!=0):#si el resto de i/2 es cero significa que es impar
+       print(i)#imprimo numero impar
+#fomra ascendente
 for i in range (numero1, numero2, -1):
     resto = i % 2
     if resto != 0:
         print(i)
+"""********************************************************************************************************
+                                    PRUEBA DE ESCRITORIO
+ **********************************************************************************************************
+  INTENTO       ENTRADA                     SALIDA
+ **********************************************************************************************************
+ *     1     *  ingrese un numero: 1      * 1                                                                                  
+ *           *  ingrese otro numero: 15   * 3
+ *           *                            * 5
+ *           *                            * 7
+ *           *                            * 9
+ *           *                            * 11
+ *           *                            * 13
+ *           *                            * 15                                          
+ * ********************************************************************************************************
+ *"""
