cb7041a1cf7163b5ef47ff4681bd0e7c5973092f
Florencia Cruz <112032348+FlorenciaCruz@users.noreply.github.com>
2022-09-20 07:23:11 -0300

    guia 2
    agrego comentarios y prueba de escritorio

--- guia 2/ejercicio3_guia2.py
+++ guia 2/ejercicio3_guia2.py
@@ -10,37 +10,47 @@ contador_minimo=int
 contador_minimo=0
 contador=int
 contador=0
-#aguinaldo=int
 aguinaldo=float
 mes_bajo=int
 maximo=None
 minimo=None
 sueldo=int
 i=int
-for i in range(0,6):
+for i in range(1,7):
     sueldo=int(input("ingrese el sueldo: "))
-    print(bool(maximo))
     if(bool(maximo)==False):#compruebo si la variable esta vacia(false si no tiene nada)
         maximo=sueldo#toma el primer valor que tenga sueldo
     if(bool(minimo)==False):#compruebo si la variable esta vacia(false si no tiene nada)
         minimo=sueldo#toma el primer valor que tenga sueldo
-        
+    #maximo sueldo    
     if(maximo<=sueldo):
         maximo=sueldo
-        print(maximo)
-        aguinaldo=maximo/2
+        aguinaldo=maximo/2#el aguinaldo es la mitad del sueldo mas alto del periodo
         mes_alto=i
-    
+    #minimo sueldo
     if(minimo>=sueldo):
         minimo=sueldo
         sueldo_minimo=sueldo
-        minimo=i
+        #mes que recibio el sueldo mas bajo
+        mes_bajo=i
 
-    acumulador_sueldo=acumulador_sueldo+sueldo
-    promedio=acumulador_sueldo/6
-print(aguinaldo)
-print("el mes que recibio el sueldo mas bajo:",minimo)
+    acumulador_sueldo=acumulador_sueldo+sueldo#acumulador de todos los sueldos igresados
+    promedio=acumulador_sueldo/6#promedio de los sueldos
+#imprimo resultados
+print("aguinaldo: ",aguinaldo)
+print("el mes que recibio el sueldo mas bajo:",mes_bajo)
 print("el total del sueldo es:",acumulador_sueldo)
 print("el promedio es:",promedio)
-
-
+"""********************************************************************************************************
+                                    PRUEBA DE ESCRITORIO
+ **********************************************************************************************************
+  INTENTO       ENTRADA                         SALIDA  
+ **********************************************************************************************************
+ *     1     * ingrese el sueldo: 1000    *      aguinaldo:  3000.0                                                                                 
+ *           * ingrese el sueldo: 2000    *      el mes que recibio el sueldo mas bajo: 1                                   
+ *           * ingrese el sueldo: 3000    *      el total del sueldo es: 21000
+ *           * ingrese el sueldo: 4000    *      el promedio es: 3500.0
+ *           * ingrese el sueldo: 5000    *
+ *           * ingrese el sueldo: 6000    *                            
+ * ********************************************************************************************************                                                                                               
+ *"""
