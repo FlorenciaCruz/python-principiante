8f048531c0576feb8dc2ed6c03cbaebc3d4faaa9
Florencia Cruz <112032348+FlorenciaCruz@users.noreply.github.com>
2022-09-22 18:02:05 -0300

    guia 2
    agrego comentarios y prueba de escritorio

--- guia 2/ejercicio6_guia2.py
+++ guia 2/ejercicio6_guia2.py
@@ -1,4 +1,20 @@
-"Mostrar por pantalla el promedio de los números ingresados por teclado. Se deja de pedir que el usuario agregue números una vez ingrese 0 (cero)."
+"""Mostrar por pantalla el promedio de los números ingresados por teclado. Se deja de pedir que el usuario agregue números una vez ingrese 0 (cero)."
+entrada
+____________________________
+numeros
+____________________________
+proceso
+____________________________
+acumulador_numeros=acumulador_numeros+numeros
+contador_numeros=contador_numeros+1
+promedio=acumulador_numeros/contador_numeros
+____________________________
+salida
+____________________________
+promedio
+contador_numeros
+____________________________"""
+#declaracion de variables
 promedio=int
 acumulador_numeros=int
 acumulador_numeros=0
@@ -6,11 +22,27 @@ contador_numeros=int
 contador_numeros=-1
 numeros=int
 numeros=1
-while(numeros!=0):
-    numeros=int(input("ingrese un numero: "))
-    acumulador_numeros=acumulador_numeros+numeros
-    contador_numeros=contador_numeros+1
+#inicio del programa
+while(numeros!=0):#termina el bucle cuando el usuario ingresa 0
+    numeros=int(input("ingrese un numero: "))#ingreso numeros por teclado
+    acumulador_numeros=acumulador_numeros+numeros#acumulador de numeros
+    contador_numeros=contador_numeros+1#contador de numeros ingresados
     
-promedio=acumulador_numeros/contador_numeros
+promedio=acumulador_numeros/contador_numeros#promedio de los numeros ingresados
+#imprimo los resultados por pantalla
 print("el promedio de los numeros es:",promedio)
-print(contador_numeros)
+print("la cantidad de numeros ingresados es de: ",contador_numeros)
+"""********************************************************************************************************
+                                    PRUEBA DE ESCRITORIO
+ **********************************************************************************************************
+  INTENTO      ENTRADA                    SALIDA   
+ **********************************************************************************************************
+ *     1     *    ingrese un numero: 2   *  el promedio de los numeros es: 8.25                                           
+ *           *    ingrese un numero: 10  *  la cantidad de numeros ingresados es de:  4
+ *           *    ingrese un numero: 20  *
+ *           *    ingrese un numero: 1   *
+ *           *    ingrese un numero: 0   *                                                                                        
+ *           *                           *                  
+ *           *                           *                 
+ * ********************************************************************************************************
+ *"""
